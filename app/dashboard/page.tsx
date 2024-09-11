090134988d24dc6e8fae5d79dd8802ef797961b6
olena kulikova <2idragonsfire@gmail.com>
2024-09-11 18:26:23 +0300

    added income table

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,34 @@
-export default function DashboardPage() {
-    return <p className="text-white">Dashboard Page</p>;
-  }
-    
+import { Card } from '@/app/ui/dashboard/cards';
+import IncomeChart from '@/app/ui/dashboard/income-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { poppins } from '@/app/ui/fonts';
+import { fetchIncome } from '@/app/lib/data';
+
+
+export default async function DashboardPage() {
+  const income = await fetchIncome();
+
+  return (
+    <main className="rounded-xl bg-neutral-900 p-6">
+      <h1
+        className={`${poppins.className} mb-4 text-center text-xl text-white md:text-3xl`}
+      >
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Earned" value={totalFulfilledInvoices} type="earned" /> */}
+        {/* <Card
+          title="In Progress"
+          value={totalAwaitingInvoices}
+          type="awaiting"
+        /> */}
+        {/* <Card title="All Invoices" value={numberOfInvoices} type="invoices" /> */}
+        {/* <Card title="Total Sellers" value={numberOfSellers} type="sellers" /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <IncomeChart income={income} />
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
+  );
+}
--- app/ui/dashboard/income-chart.tsx
+++ app/ui/dashboard/income-chart.tsx
@@ -3,15 +3,15 @@ import { CalendarDaysIcon } from '@heroicons/react/24/outline';
 import { poppins } from '@/app/ui/fonts';
 import { Income } from '@/app/lib/definitions';
 
-export default async function IncomeChart() {
+export default async function IncomeChart({ income }: { income: Income[] }) {
   const chartHeight = 350;
   // Attention! Uncomment this section when you reach this stage in the course.
 
-  // const { yAxisLabels, topLabel } = generateYAxis(income);
+  const { yAxisLabels, topLabel } = generateYAxis(income);
 
-  // if (!income || income.length === 0) {
-  //   return <p className="mt-4 text-gray-400">No data available.</p>;
-  // }
+  if (!income || income.length === 0) {
+    return <p className="mt-4 text-gray-400">No data available.</p>;
+  }
 
   return (
     <div className="w-full md:col-span-4">
@@ -22,7 +22,7 @@ export default async function IncomeChart() {
       </h2>
       {/* Attention! Uncomment this section when you reach this stage in the course. */}
 
-      {/* <div className="rounded-xl bg-neutral-700 p-4">
+      <div className="rounded-xl bg-neutral-700 p-4">
         <div className="mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-neutral-700 p-4 sm:grid-cols-13 md:gap-4">
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-white sm:flex"
@@ -51,7 +51,7 @@ export default async function IncomeChart() {
           <CalendarDaysIcon className="h-5 w-5 text-white" />
           <h3 className="ml-2 text-sm text-white ">This year</h3>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
