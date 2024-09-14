70829f679a4ae70de24b7ba635bee706e419a70a
olena kulikova <2idragonsfire@gmail.com>
2024-09-14 16:59:00 +0300

    added latest invoices and cards information

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,11 +2,22 @@ import { Card } from '@/app/ui/dashboard/cards';
 import IncomeChart from '@/app/ui/dashboard/income-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { poppins } from '@/app/ui/fonts';
-import { fetchIncome } from '@/app/lib/data';
+import { 
+  fetchIncome, 
+  fetchLatestInvoices, 
+  fetchCardData,
+} from '@/app/lib/data';
 
 
 export default async function DashboardPage() {
   const income = await fetchIncome();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    numberOfSellers,
+    numberOfInvoices,
+    totalFulfilledInvoices,
+    totalAwaitingInvoices,
+  } = await fetchCardData();
 
   return (
     <main className="rounded-xl bg-neutral-900 p-6">
@@ -16,18 +27,18 @@ export default async function DashboardPage() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Earned" value={totalFulfilledInvoices} type="earned" /> */}
-        {/* <Card
+        <Card title="Earned" value={totalFulfilledInvoices} type="earned" />
+        <Card
           title="In Progress"
           value={totalAwaitingInvoices}
           type="awaiting"
-        /> */}
-        {/* <Card title="All Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card title="Total Sellers" value={numberOfSellers} type="sellers" /> */}
+        />
+        <Card title="All Invoices" value={numberOfInvoices} type="invoices" />
+        <Card title="Total Sellers" value={numberOfSellers} type="sellers" />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <IncomeChart income={income} />
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -19,7 +19,7 @@ export default async function LatestInvoices({
       <div className="flex grow flex-col justify-between rounded-xl bg-neutral-700 p-4">
         {/* Attention! Uncomment this section when you reach this stage in the course. */}
 
-        {/* <div className="bg-neutral-00 px-6">
+        <div className="bg-neutral-00 px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -56,7 +56,7 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
+        </div>
 
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-white" />
