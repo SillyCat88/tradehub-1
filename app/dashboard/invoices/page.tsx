c7fb23ab072f6ba8c5b68b97ed843e2184789bf7
olena kulikova <2idragonsfire@gmail.com>
2024-09-15 19:08:35 +0300

    created invoices page with a search field

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,32 @@
-export default function InvoicePage() {
-    return <p className="text-white">Invoices Page</p>;
-  }
-    
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { poppins } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+
+export default async function InvoicePage() {
+  return (
+    <div className="w-full rounded-xl bg-neutral-900 p-6">
+      <div className="flex w-full items-center justify-center">
+        <h1
+          className={`${poppins.className} text-center text-xl text-white md:text-3xl`}
+        >
+          Invoices
+        </h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Find invoice" />
+        <CreateInvoice />
+      </div>
+      {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
+}
+  
