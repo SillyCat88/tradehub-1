68bff9690978b0486b69918ddffa7531a63f78da
olena kulikova <2idragonsfire@gmail.com>
2024-09-17 14:56:24 +0300

    created the pagination for invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { poppins } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 export default async function InvoicePage({
   searchParams,
@@ -16,6 +17,8 @@ export default async function InvoicePage({
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full rounded-xl bg-neutral-900 p-6">
@@ -34,7 +37,7 @@ export default async function InvoicePage({
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -14,6 +14,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   
   function handleSearch (term: string) {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     
     if (term) {
       params.set('query', term);
