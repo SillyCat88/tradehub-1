b0d2b9e206cd5374dc791001918e41e273be7581
olena kulikova <2idragonsfire@gmail.com>
2024-09-23 15:51:03 +0300

    working on updateInvoice, parse id, bind id

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,6 +15,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, data: true });
 
 export async function createInvoice(formData: FormData) {
     const { sellerId, amount, status } = CreateInvoice.parse ({
@@ -34,4 +35,21 @@ export async function createInvoice(formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
+export async function updateInvoice(id: string, formData: FormData) {
+    const { sellerId, amount, status } = UpdateInvoice.parse ({
+        sellerId: formData.get('sellerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+
+    await sql`
+        UPDATE invoices
+        SET seller_id = ${sellerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
   
