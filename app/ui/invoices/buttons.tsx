0594ec14e749972aa1996b058c097a27e74a70e1
olena kulikova <2idragonsfire@gmail.com>
2024-09-23 19:26:34 +0300

    added delete invoice functionality

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -4,6 +4,7 @@ import {
   TrashIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { deleteInvoice } from '@/app/lib/actions';
 
 export function CreateInvoice() {
   return (
@@ -29,12 +30,14 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+
   return (
-    <>
+    <form action={deleteInvoiceWithId}>
       <button className="rounded-md border bg-sky-700 p-2 text-white hover:bg-white hover:text-neutral-900">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
-    </>
+    </form>
   );
 }
