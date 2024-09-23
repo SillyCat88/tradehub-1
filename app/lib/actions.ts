646903a4c627d8a9d2010369b701650c56db3f86
olena kulikova <2idragonsfire@gmail.com>
2024-09-23 16:25:24 +0300

    fixed the typo

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,7 +15,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-const UpdateInvoice = FormSchema.omit({ id: true, data: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
     const { sellerId, amount, status } = CreateInvoice.parse ({
