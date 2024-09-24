84841b68c70dd89b25999f0bc424148f1c6f1796
olena kulikova <2idragonsfire@gmail.com>
2024-09-24 13:44:17 +0300

    added required to input and select in create form

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -26,6 +26,7 @@ export default function Form({ sellers }: { sellers: SellerField[] }) {
               name="sellerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              required
             >
               <option value="" disabled>
                 Select a seller
@@ -56,6 +57,7 @@ export default function Form({ sellers }: { sellers: SellerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                required
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -75,6 +77,7 @@ export default function Form({ sellers }: { sellers: SellerField[] }) {
                   type="radio"
                   value="awaiting"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  required
                 />
                 <label
                   htmlFor="awaiting"
@@ -90,6 +93,7 @@ export default function Form({ sellers }: { sellers: SellerField[] }) {
                   type="radio"
                   value="fulfilled"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  required
                 />
                 <label
                   htmlFor="fulfilled"
